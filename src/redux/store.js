0f9db6c6b6bd5c93148d7bbbbeda7a1ee7f5e9cf
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2019-10-15 10:49:43 +0200

    redux persists added to project

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,35 @@
 import React from 'react';
+import { AsyncStorage } from 'react-native';
 
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import rootReducer from '../redux/reducers';
 
 import Navigation from '../navigation';
 
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
 const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  persistedReducer,
+  composeEnhancers
 )
+
+const persistor = persistStore(store)
+
 const App = () => (
-  <Provider store = { store }>
-    <Navigation />
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Navigation />
+    </PersistGate>
   </Provider>
 );
 export default App;
